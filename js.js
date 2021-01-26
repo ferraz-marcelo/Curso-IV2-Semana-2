09a20d81722cd50ea0e9f0239f389da9676bfcb6
ferraz-marcelo <77532227+ferraz-marcelo@users.noreply.github.com>
2021-01-25 21:28:51 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,9 @@
+var paginaToda = document.querySelector('body')
+
+function mudaTema() {
+  if (paginaToda .className == 'claro') {
+    paginaToda .className = 'escuro'
+  } else {
+    paginaToda .className = 'claro'
+  }
+}
